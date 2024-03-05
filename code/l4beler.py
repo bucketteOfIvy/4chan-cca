6ea064d3fd7b0db76eb9e82764bd6f6dda1783b5
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-03-05 16:57:02 -0600

    added updated l4beler

--- code/l4beler.py
+++ code/l4beler.py
@@ -10,8 +10,6 @@
 
 import HelperChan as hc
 import pandas as pd
-# TODO: make this funnier iff you plan on using it on others 
-print('''Welcome to l4beler.py, the world's most innovative 4chan post labeling module (\j).''')
 
 def limited_input(prompt, is_valid_response=lambda x: (x in ['0', '1']), condition='Either 0 or 1.'):
     '''
@@ -35,41 +33,50 @@ def is_pos_int(x):
     except:
         return False
 
-num_to_grab = limited_input('How many posts would you like to label?\n', 
-                            is_valid_response=is_pos_int, 
-                            condition='is positive integer')
+if __name__ == '__main__':
+    
+    # TODO: make this funnier iff you plan on using it on others 
+    print('''Welcome to l4beler.py, the world's most innovative 4chan post labeling module (\j).''')
 
-print(f"Awesome, I'll grab {int(num_to_grab)} posts!")
+    num_to_grab = limited_input('How many posts would you like to label?\n', 
+                                is_valid_response=is_pos_int, 
+                                condition='is positive integer')
 
-print(f"Grabbing {int(num_to_grab)} random posts..")
+    print(f"Awesome, I'll grab {int(num_to_grab)} posts!")
 
-# Read and subset
-posts = pd.read_csv('../data/lgbt_week_1.csv')
-sampled_posts = posts.sample(n=int(num_to_grab))
+    print(f"Grabbing {int(num_to_grab)} random posts..")
 
-print(f'{len(sampled_posts)} posts grabbed! Prepare yourself for ~classifying~')
+    week_num = limited_input('Which week\'s data?', 
+                                is_valid_response=lambda x: True if str(x) in ['1', '2', '3'] else False,
+                                condition = 'one of 1, 2, or 3')
+    
+    # Read and subset
+    posts = pd.read_csv(f'../data/lgbt_week_{week_num}.csv')
+    sampled_posts = posts.sample(n=int(num_to_grab))
 
-classes = {}
-for ind, post in sampled_posts.iterrows():
-    print('-' * 20)
+    print(f'{len(sampled_posts)} posts grabbed! Prepare yourself for ~classifying~')
+
+    classes = {}
+    for ind, post in sampled_posts.iterrows():
+        print('-' * 20)
+        try:
+            backref = hc.content_with_back_reference(post['content'], posts)
+        except TypeError:
+            # fix this later -.-
+            print("Failed to retrieve proper backref. Presenting post without context.")
+            backref = post['content']
+        print(backref)
+        rsp = limited_input('\n:: If this is a trans related post, type "1". Otherwise, type "0". ::\n')
+        classes[ind] = int(rsp)
+
+    print(classes)
+    cldf = pd.DataFrame({'classification':classes.values()}, index=classes.keys())
+    print(cldf)
+    sampled_posts = sampled_posts.merge(cldf, left_index=True, right_index=True)
+
+    filename = input("""What would you like the filename of your classification to be?
+                    Note that this will already save in the ../data/ directory.\n""")
     try:
-        backref = hc.content_with_back_reference(post['content'], posts)
-    except TypeError:
-        # fix this later -.-
-        print("Failed to retrieve proper backref. Presenting post without context.")
-        backref = post['content']
-    print(backref)
-    rsp = limited_input('\n:: If this is a trans related post, type "1". Otherwise, type "0". ::\n')
-    classes[ind] = int(rsp)
-
-print(classes)
-cldf = pd.DataFrame({'classification':classes.values()}, index=classes.keys())
-print(cldf)
-sampled_posts = sampled_posts.merge(cldf, left_index=True, right_index=True)
-
-filename = input("""What would you like the filename of your classification to be?
-                  Note that this will already save in the ../data/ directory.\n""")
-try:
-    sampled_posts.to_csv(f'../data/{filename}')
-except:
-     sampled_posts.to_csv('../data/woops_there_was_an_error_lol.csv')
+        sampled_posts.to_csv(f'../data/{filename}')
+    except:
+        sampled_posts.to_csv('../data/woops_there_was_an_error_lol.csv')
